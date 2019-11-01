d45cbb54ab17207f4abf9661d8e4419710dff8ed
Timothy Cao <tcao.pro@gmail.com>
2019-11-01 17:45:25 -0400

    firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDuzRFuq6DyrVEvgtB9CzwZYcgZAgIppbE",
+    authDomain: "tc-organic-market.firebaseapp.com",
+    databaseURL: "https://tc-organic-market.firebaseio.com",
+    projectId: "tc-organic-market",
+    storageBucket: "tc-organic-market.appspot.com",
+    messagingSenderId: "195115099940",
+    appId: "1:195115099940:web:b5c137d6933a03224bf5b1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDuzRFuq6DyrVEvgtB9CzwZYcgZAgIppbE",
+    authDomain: "tc-organic-market.firebaseapp.com",
+    databaseURL: "https://tc-organic-market.firebaseio.com",
+    projectId: "tc-organic-market",
+    storageBucket: "tc-organic-market.appspot.com",
+    messagingSenderId: "195115099940",
+    appId: "1:195115099940:web:b5c137d6933a03224bf5b1"
+  }
 };
 
 /*
